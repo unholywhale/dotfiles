c0f9c882da5336850d6bfb385192e26b90cb7459
Alex Zheleznov <unholywhale@gmail.com>
2024-04-06 17:43:49 +0700

    Update NixOS config

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -3,7 +3,7 @@
 {
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   imports =
-    [ # Include the results of the hardware scan.
+    [ 
       ./hardware-configuration.nix
     ];
 
@@ -14,15 +14,15 @@
   boot.loader.grub.useOSProber = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # OpenGL
+  # Enable OpenGL
   hardware.opengl = {
     enable = true;
     driSupport = true;
     driSupport32Bit = true;
   };
 
+  # NVIDIA drivers
   services.xserver.videoDrivers = ["nvidia"];
-
   hardware.nvidia = {
     modesetting.enable = true;
     powerManagement.enable = true;
@@ -32,83 +32,49 @@
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
+  # Enable X Server and Desktop Environment
   services.xserver.enable = true;
   services.xserver.displayManager.sddm.enable = true;
+  services.xserver.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
+  programs.hyprland.enable = true;
 
-  environment.systemPackages = with pkgs; [
-    oh-my-zsh
-    zsh
-    zsh-completions
-    zsh-syntax-highlighting
-    rofi-wayland
-    rofi-power-menu
-    waybar
-    dunst
-    libnotify
-    swww
-    grimblast
-    kitty
-    networkmanagerapplet
-    killall
-  ];
-
-  programs.thunar.enable = true;
-
-  programs.zsh = {
-    enable = true;
-    ohMyZsh = {
-      enable = true;
-      theme = "robbyrussell";
-      plugins = [
-        "sudo"
-        "terraform"
-        "systemadmin"
-      ];
-    };    
-  };
-  
-  programs.hyprland = {
-    enable = true;
-    xwayland.enable = true;
-  };
-
-  xdg.portal.enable = true;
-
-  environment.sessionVariables = {
-    # No cursor fix
-    WLR_NO_HARDWARE_CURSORS = "1";
-    # Hint electron apps to use wayland
-    NIXOS_OZONE_WL = "1";
-  };
-  
-  networking.hostName = "nixos"; # Define your hostname.
-  
   # Enable networking
+  networking.hostName = "nixos"; # Define your hostname.
   networking.networkmanager.enable = true;
 
   # Bluetooth
-  hardware.bluetooth.enable = true; # enables support for Bluetooth
-  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+  hardware.bluetooth = {
+    enable = true; 
+    powerOnBoot = true;
+    settings.General.ControllerMode = "bredr";
+  };
   services.blueman.enable = true;
+
+  # Configure PipeWire
   hardware.pulseaudio.enable = false;
-  services.pipewire.wireplumber.configPackages = [
-	  (pkgs.writeTextDir "share/wireplumber/bluetooth.lua.d/51-bluez-config.lua" ''
-		bluez_monitor.properties = {
-			["bluez5.enable-sbc-xq"] = true,
-			["bluez5.enable-msbc"] = true,
-			["bluez5.enable-hw-volume"] = true,
-			["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
-    }
-	'')
-  ];
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    wireplumber.configPackages = [
+	    (pkgs.writeTextDir "share/wireplumber/bluetooth.lua.d/51-bluez-config.lua" ''
+      	bluez_monitor.properties = {
+			    ["bluez5.enable-sbc-xq"] = true,
+			    ["bluez5.enable-msbc"] = true,
+			    ["bluez5.enable-hw-volume"] = true,
+			    ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+        }
+      '')
+    ];
+  };
   
   # Set your time zone.
   time.timeZone = "Asia/Bangkok";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "ru_RU.UTF-8";
     LC_IDENTIFICATION = "ru_RU.UTF-8";
@@ -128,17 +94,11 @@
     options = "ctrl:nocaps";
   };
 
-  # rtkit is optional but recommended
+  # Enable rtkit
   security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-  };
 
+  # Enable zsh
+  programs.zsh.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.alex = {
@@ -149,26 +109,12 @@
     shell = pkgs.zsh;
   };
 
-  fonts.packages = with pkgs; [
-    noto-fonts
-    noto-fonts-cjk
-    noto-fonts-emoji
-    liberation_ttf
-    fira-code
-    fira-code-symbols
-    jetbrains-mono
-    mplus-outline-fonts.githubRelease
-    dina-font
-    proggyfonts
-    nerdfonts
-  ];
-
-  # Enable automatic login for the user.
-  services.getty.autologinUser = "alex";
-
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  system.stateVersion = "23.11"; 
+  system.stateVersion = "23.11";   
+
+  # Enable automatic login for the user.
+  # services.getty.autologinUser = "alex";
 
 }
--- nixos/home.nix
+++ nixos/home.nix
@@ -1,5 +1,3 @@
-# This is your home-manager configuration file
-# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
 {
   inputs,
   lib,
@@ -7,19 +5,13 @@
   pkgs,
   ...
 }: {
-  # You can import other home-manager modules here
   imports = [
-    # If you want to use home-manager modules from other flakes (such as nix-colors):
-    # inputs.nix-colors.homeManagerModule
-    # You can also split up your configuration and import pieces of it here:
-    # ./nvim.nix
   ];
 
   home = {
     username = "alex";
     homeDirectory = "/home/alex";
   };
-  
 
   home.packages = with pkgs; [
     manix
@@ -28,33 +20,95 @@
     lxappearance
     vim
     nicotine-plus
+    oh-my-zsh
+    zsh
+    zsh-completions
+    zsh-syntax-highlighting
+    rofi-wayland
+    rofi-power-menu
+    waybar
+    dunst
+    libnotify
+    swww
+    grimblast
+    kitty
+    networkmanagerapplet
+    killall
+    file
+    gcc
+    python3
+    # Fonts
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+    liberation_ttf
+    fira-code
+    fira-code-symbols
+    jetbrains-mono
+    mplus-outline-fonts.githubRelease
+    dina-font
+    nerdfonts
   ];
+
+  programs.zsh = {
+    enable = true;
+    oh-my-zsh = {
+      enable = true;
+      theme = "robbyrussell";
+      plugins = [
+        "sudo"
+        "terraform"
+        "systemadmin"
+      ];
+    };
+  };
+
+  wayland.windowManager.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+    extraConfig = "source=~/dotfiles/.config/hypr/hyprland.conf";
+  };
+
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = with pkgs; [
+    xdg-desktop-portal-kde
+  ];
+  xdg.portal.config.common.default = "*";
+  
+  home.sessionVariables = {
+    WLR_NO_HARDWARE_CURSORS = "1";
+    NIXOS_OZONE_WL = "1";
+  };
 
   gtk = {
     enable = true;
-    theme = {
-      name = "Catppuccin-Macchiato-Compact-Pink-Dark";
-      package = pkgs.catppuccin-gtk.override {
-        accents = [ "lavender" ];
-        size = "compact";
-        tweaks = [ "rimless" ];
-        variant = "mocha";
-      };
-    };
-  };
-  xdg.configFile = {
-    "gtk-4.0/assets".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
-    "gtk-4.0/gtk.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
-    "gtk-4.0/gtk-dark.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
-  };
-  home.pointerCursor = {
-    gtk.enable = true;
-    name = "Catppuccin-Mocha-Light-Cursors";
-    package = pkgs.catppuccin-cursors.mochaLight;
-    size = 16;
+    # theme = {
+    #   name = "Catppuccin-Macchiato-Compact-Pink-Dark";
+    #   package = pkgs.catppuccin-gtk.override {
+    #     accents = [ "lavender" ];
+    #     size = "compact";
+    #     tweaks = [ "rimless" ];
+    #     variant = "mocha";
+    #   };
+    # };
   };
 
-  programs.wezterm.enable = true;
+  fonts.fontconfig.enable = true;
+  
+  # xdg.configFile = {
+  #   "gtk-4.0/assets".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
+  #   "gtk-4.0/gtk.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
+  #   "gtk-4.0/gtk-dark.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
+  # };
+  
+  # home.pointerCursor = {
+  #   gtk.enable = true;
+  #   name = "Catppuccin-Mocha-Light-Cursors";
+  #   package = pkgs.catppuccin-cursors.mochaLight;
+  #   size = 16;
+  # };
+
+  #programs.wezterm.enable = true;
   
   programs.emacs = {
     enable = true;
@@ -64,14 +118,12 @@
       copilot
     ];
   };
-  services.emacs = {
-    enable = true;
-  };
+  services.emacs.enable = true;
   home.file.".emacs".text = ''(load "~/dotfiles/.emacs")'';
 
-  home.file.".config/hypr/hyprland.conf".text = ''
-    source=~/dotfiles/.config/hypr/hyprland.conf
-  '';
+  # home.file.".config/hypr/hyprland.conf".text = ''
+  #   source=~/dotfiles/.config/hypr/hyprland.conf
+  # '';
 
   programs.firefox.enable = true;
 
@@ -82,19 +134,19 @@
     userEmail = "unholywhale@gmail.com";
   };
  
-  dconf.settings = {
-    "org/gnome/desktop/wm/preferences" = {
-      focus-new-windows = "smart";
-      auto-raise = "true";
-    };
-    "org/gnome/shell" = {
-      enabled-extensions = [
-        "dash-to-dock@micxgx.gmail.com"
-        "apps-menu"
-        "places-menu"
-      ];
-    };
-  };
+  # dconf.settings = {
+  #   "org/gnome/desktop/wm/preferences" = {
+  #     focus-new-windows = "smart";
+  #     auto-raise = "true";
+  #   };
+  #   "org/gnome/shell" = {
+  #     enabled-extensions = [
+  #       "dash-to-dock@micxgx.gmail.com"
+  #       "apps-menu"
+  #       "places-menu"
+  #     ];
+  #   };
+  # };
   # gsettings set org.gnome.desktop.wm.preferences focus-new-windows 'smart'
 
   programs.home-manager.enable = true;
