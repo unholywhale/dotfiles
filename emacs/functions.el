bb31577e1bad2fca1208e6078915cec94b538318
unholywhale <unholywhale@gmail.com>
2025-07-04 00:34:24 +0800

    Add killing quoted blocks

--- emacs/functions.el
+++ emacs/functions.el
@@ -56,22 +56,49 @@
   "Kill forward word or whitespace sequence (Vim-like dw behavior)."
   (interactive)
   (let ((start (point)))
-    (if (looking-at "\\s-\\{2,\\}")
-        ;; If at 2+ whitespaces, kill all whitespace
-        (skip-chars-forward " \t\n")
-      ;; Otherwise, kill to end of word (including single spaces)
-      (forward-word))
+    (cond
+     ;; If at 2+ whitespaces, kill all whitespace
+     ((looking-at "\\s-\\{2,\\}")
+      (skip-chars-forward " \t\n"))
+     ;; If at a quote, kill the entire quoted string
+     ((looking-at "[\"']")
+      (let ((quote-char (char-after)))
+        (forward-char) ; move past opening quote
+        (while (and (not (eobp))
+                    (not (= (char-after) quote-char)))
+          (if (= (char-after) ?\\)
+              (forward-char 2) ; skip escaped character
+            (forward-char)))
+        (when (= (char-after) quote-char)
+          (forward-char)))) ; include closing quote
+     ;; Otherwise, kill to end of word (including single spaces)
+     (t
+      (forward-word)))
     (kill-region start (point))))
 
 (defun my/backward-kill-word-or-whitespace ()
   "Kill backward word or whitespace sequence."
   (interactive)
   (let ((end (point)))
-    (if (looking-back "\\s-\\{2,\\}" (line-beginning-position))
-        ;; If preceded by 2+ whitespaces, kill all whitespace backward
-        (skip-chars-backward " \t\n")
-      ;; Otherwise, kill to beginning of word
-      (backward-word))
+    (cond
+     ;; If preceded by 2+ whitespaces, kill all whitespace backward
+     ((looking-back "\\s-\\{2,\\}" (line-beginning-position))
+      (skip-chars-backward " \t\n"))
+     ;; If at a quote (looking back), kill the entire quoted string
+     ((looking-back "[\"']" (line-beginning-position))
+      (let ((quote-char (char-before)))
+        (backward-char) ; move past closing quote
+        (while (and (not (bobp))
+                    (not (and (= (char-before) quote-char)
+                              ;; Check if this quote is not escaped
+                              (or (= (point) (line-beginning-position))
+                                  (not (= (char-before (1- (point))) ?\\))))))
+          (backward-char))
+        (when (and (not (bobp)) (= (char-before) quote-char))
+          (backward-char)))) ; include opening quote
+     ;; Otherwise, kill to beginning of word
+     (t
+      (backward-word)))
     (kill-region (point) end)))
 
 (provide 'functions)
